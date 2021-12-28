e8f4381c5f016051341955c19fe8667a11d31d1f
Sanford Pun <punsanford@gmail.com>
2021-12-28 15:29:41 +0800

    Set zoom levels to [0.1, 10]

--- freeport/frontend/src/zoom.js
+++ freeport/frontend/src/zoom.js
@@ -3,7 +3,7 @@ import * as d3 from "d3";
 export function addZoomBehavior(element, container, width, height) {
     container.call(d3.zoom()
         .extent([[0, 0], [width, height]])
-        .scaleExtent([1, 8])
+        .scaleExtent([0.1, 10])
         .on("zoom", ({ transform }) => {
             element.attr("transform", transform)
         })
