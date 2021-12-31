85008653ca8447e74a8fc02bbb0b9eb1c99050ed
Sanford Pun <punsanford@gmail.com>
2021-12-31 17:25:57 +0800

    Scraping utilities

--- freeport/backend/index.js
+++ freeport/backend/index.js
@@ -1,3 +1,7 @@
-const { test_api } = require("./src/api_access/test");
+// const { test_api } = require("./src/api_access/test");
+// test_api().catch(console.error);
 
-test_api().catch(console.error);
+const { test_scrap } = require("./src/scrap/test");
+test_scrap({
+    log: true
+}).catch(console.error);
