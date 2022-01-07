7914222ea21d4636971c84beca3118ea9a47cdfb
Sanford Pun <punsanford@gmail.com>
2022-01-07 16:35:57 +0800

    Use "number" type for number inputs

--- freeport/frontend/src/gui/input.js
+++ freeport/frontend/src/gui/input.js
@@ -12,8 +12,15 @@ export const Input = inputElemIds.reduce((obj, id) => {
     const elem = obj[id].elem = document.getElementById(id);
 
     if (elem !== null) {
-        obj[id].parseInt = () => parseInt(elem.value);
-        obj[id].parseString = () => elem.value;
+        switch (obj[id].elem.type) {
+            case "number":
+                obj[id].parseInt = () => parseInt(elem.value);
+                break;
+            case "text":
+                obj[id].parseString = () => elem.value;
+                break;
+            default:
+        }
     }
 
     return obj;
