5df58d2b02e5ea45d02dc8de917a2c6c3345dba7
Sanford Pun <punsanford@gmail.com>
2022-01-17 15:02:17 +0800

    Unit test connectivity

--- starfish/tests/mod.rs
+++ starfish/tests/mod.rs
@@ -1,18 +1,19 @@
 mod common;
 
 use common::TestContext;
-use sea_orm::{DbConn, DbErr};
+use sea_orm::{DbConn, DbErr, FromQueryResult, ConnectionTrait};
+use sea_query::Alias;
 use starfish::{
     api::db::schema::create_tables,
     core::entities::entity_attribute::Datatype,
-    mutate::{ClearEdgeJson, EdgeJson, Mutate, NodeJson},
+    mutate::{ClearEdgeJson, EdgeJson, Mutate, NodeJson, NodeJsonBatch, Node, EdgeJsonBatch, Edge},
     schema::{EntityAttrJson, EntityJson, RelationJson, Schema},
 };
 use std::collections::HashMap;
 
 #[smol_potat::test]
 async fn main() -> Result<(), DbErr> {
-    let ctx = TestContext::new("starfish_tests").await;
+    let ctx = TestContext::new("starfish_tests_main").await;
     let db = &ctx.db;
 
     create_tables(db).await?;
@@ -24,6 +25,29 @@ async fn main() -> Result<(), DbErr> {
     test_insert_edge(db).await?;
     test_delete_edge(db).await?;
     test_clear_edge(db).await?;
+
+    Ok(())
+}
+
+#[smol_potat::test]
+async fn connectivity() -> Result<(), DbErr> {
+    let ctx = TestContext::new("starfish_tests_connectivity").await;
+    let db = &ctx.db;
+
+    create_tables(db).await?;
+
+    test_create_entities(db).await?;
+    test_create_relations(db).await?;
+
+    let correct_nodes = test_construct_mock_graph(db).await?;
+    Mutate::calculate_compound_connectivity(db).await?;
+
+    let nodes = test_get_nodes_with_connectivity(db).await?;
+    assert_eq!(nodes.len(), 6);
+
+    for (name, node) in nodes {
+        assert_eq!(node, *correct_nodes.get(&name).unwrap());
+    }
 
     Ok(())
 }
@@ -176,4 +200,80 @@ async fn test_clear_edge(db: &DbConn) -> Result<(), DbErr> {
     .await?;
 
     Ok(())
+}
+
+#[derive(Debug, Clone, Eq, FromQueryResult, PartialEq)]
+struct TestNode {
+    name: String,
+    in_conn: i32,
+    in_conn_compound: i32,
+}
+
+async fn test_construct_mock_graph(db: &DbConn) -> Result<HashMap<String, TestNode>, DbErr> {
+    Mutate::insert_node_batch(
+        db,
+        NodeJsonBatch {
+            of: "crate".to_owned(),
+            nodes: vec![
+                Node { name: "A".to_owned(), attributes: HashMap::new() },
+                Node { name: "B".to_owned(), attributes: HashMap::new() },
+                Node { name: "C".to_owned(), attributes: HashMap::new() },
+                Node { name: "D".to_owned(), attributes: HashMap::new() },
+                Node { name: "E".to_owned(), attributes: HashMap::new() },
+                Node { name: "F".to_owned(), attributes: HashMap::new() },
+            ],
+        }
+    )
+    .await?;
+
+    Mutate::insert_edge_batch(
+        db,
+        EdgeJsonBatch {
+            name: "depends".to_owned(),
+            edges: vec![
+                Edge { from_node: "A".to_owned(), to_node: "C".to_owned() },
+                Edge { from_node: "B".to_owned(), to_node: "C".to_owned() },
+                Edge { from_node: "B".to_owned(), to_node: "D".to_owned() },
+                Edge { from_node: "C".to_owned(), to_node: "E".to_owned() },
+                Edge { from_node: "D".to_owned(), to_node: "E".to_owned() },
+                Edge { from_node: "D".to_owned(), to_node: "F".to_owned() },
+            ],
+        }
+    )
+    .await?;
+
+    Ok(
+        HashMap::from([
+            ("A".to_owned(), TestNode { name: "A".to_owned(), in_conn: 0, in_conn_compound: 0 }),
+            ("B".to_owned(), TestNode { name: "B".to_owned(), in_conn: 0, in_conn_compound: 0 }),
+            ("C".to_owned(), TestNode { name: "C".to_owned(), in_conn: 2, in_conn_compound: 2 }),
+            ("D".to_owned(), TestNode { name: "D".to_owned(), in_conn: 1, in_conn_compound: 1 }),
+            ("E".to_owned(), TestNode { name: "E".to_owned(), in_conn: 2, in_conn_compound: 4 }),
+            ("F".to_owned(), TestNode { name: "F".to_owned(), in_conn: 1, in_conn_compound: 2 }),
+        ])
+    )
+}
+
+async fn test_get_nodes_with_connectivity(db: &DbConn) -> Result<HashMap<String, TestNode>, DbErr> {
+    let builder = db.get_database_backend();
+    Ok(
+        TestNode::find_by_statement(
+            builder.build(
+                sea_query::Query::select()
+                    .columns([
+                        Alias::new("name"),
+                        Alias::new("in_conn"),
+                        Alias::new("in_conn_compound")
+                    ])
+                    .from(Alias::new("node_crate"))
+            )
+        )
+        .all(db)
+        .await?
+        .into_iter()
+        .fold(HashMap::new(), |mut map, node| {
+            map.insert(node.name.clone(), node);
+            map
+        })
+    )
 }
