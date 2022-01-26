c4d4040993e69bb036a15a4010ae2001bdffedfa
Sanford Pun <punsanford@gmail.com>
2022-01-26 16:14:32 +0800

    Improve unit tests

--- starfish/tests/core.rs
+++ starfish/tests/core.rs
@@ -3,6 +3,7 @@ mod common;
 use std::collections::HashMap;
 
 use common::TestContext;
+use migration::sea_orm::{ConnectionTrait, DbConn, FromQueryResult};
 use migration::{Migrator, MigratorTrait, SchemaManager};
 use sea_orm::DbErr;
 use starfish_core::lang::{
@@ -10,7 +11,9 @@ use starfish_core::lang::{
     MutateInsertJson, MutateJson, MutateNodeSelectorJson, MutateUpdateJson, Node, NodeJsonBatch,
 };
 use starfish_core::mutate::Mutate;
+use starfish_core::schema::{format_edge_table_name, format_node_table_name};
 use starfish_core::sea_orm;
+use starfish_core::sea_query::{Alias, Cond, Expr, Query};
 use starfish_core::{
     entities::entity_attribute::Datatype,
     lang::{EntityAttrJson, EntityJson, RelationJson, SchemaDefineJson, SchemaJson},
@@ -70,7 +73,15 @@ async fn main() -> Result<(), DbErr> {
     }));
 
     Mutate::mutate(db, mutate_json, false).await?;
-    // New node should be inserted (name: "sea-orm", version: "1.0")
+
+    {
+        // New node should be inserted (name: "sea-orm", version: "1.0")
+        let sea_orm = TestNode::get_with_name(db, "crate", "sea-orm")
+            .await?
+            .unwrap();
+        assert_eq!(sea_orm.name, "sea-orm");
+        assert_eq!(sea_orm.attr_version, "1.0");
+    }
 
     let mutate_json = MutateJson::insert(MutateInsertJson::node(NodeJsonBatch {
         of: "crate".to_owned(),
@@ -90,8 +101,20 @@ async fn main() -> Result<(), DbErr> {
         .await
         .is_err());
     Mutate::mutate(db, mutate_json, true).await?;
-    // New node should be inserted (name: "sea-query", version: "1.0")
-    // Node should be updated (name: "sea-orm", version: "2.0")
+    {
+        // New node should be inserted (name: "sea-query", version: "1.0")
+        // Node should be updated (name: "sea-orm", version: "2.0")
+        let sea_query = TestNode::get_with_name(db, "crate", "sea-query")
+            .await?
+            .unwrap();
+        assert_eq!(sea_query.name, "sea-query");
+        assert_eq!(sea_query.attr_version, "1.0");
+        let sea_orm = TestNode::get_with_name(db, "crate", "sea-orm")
+            .await?
+            .unwrap();
+        assert_eq!(sea_orm.name, "sea-orm");
+        assert_eq!(sea_orm.attr_version, "2.0");
+    }
 
     let mutate_json = MutateJson::insert(MutateInsertJson::edge(EdgeJsonBatch {
         of: "depends".to_owned(),
@@ -102,9 +125,24 @@ async fn main() -> Result<(), DbErr> {
     }));
 
     Mutate::mutate(db, mutate_json.clone(), false).await?;
-    // New edge should be inserted (from_node: "sea-orm", to_node: "sea-query")
+    {
+        // New edge should be inserted (from_node: "sea-orm", to_node: "sea-query")
+        assert!(
+            TestEdge::get_with_from_to(db, "depends", "sea-orm", "sea-query")
+                .await?
+                .is_some()
+        );
+        assert!(
+            TestEdge::get_with_from_to(db, "depends", "sea-query", "sea-orm")
+                .await?
+                .is_none()
+        );
+    }
     Mutate::mutate(db, mutate_json, false).await?;
-    // NO new edge should be inserted
+    {
+        // NO new edge should be inserted
+        assert_eq!(TestEdge::get_all(db, "depends").await?.len(), 1);
+    }
 
     println!("# Node and edges inserted successfully! #");
 
@@ -118,7 +156,13 @@ async fn main() -> Result<(), DbErr> {
     });
 
     Mutate::mutate(db, mutate_json, false).await?;
-    // Node should be updated (name: "sea-orm", version: "3.14")
+    {
+        // Node should be updated (name: "sea-orm", version: "3.14")
+        let sea_orm = TestNode::get_with_name(db, "crate", "sea-orm")
+            .await?
+            .unwrap();
+        assert_eq!(sea_orm.attr_version, "3.14");
+    }
 
     let mutate_json = MutateJson::update(MutateUpdateJson::edge {
         selector: MutateEdgeSelectorJson {
@@ -135,8 +179,16 @@ async fn main() -> Result<(), DbErr> {
     });
 
     Mutate::mutate(db, mutate_json, false).await?;
-    // Edge should be updated (from_node: "sea-query", to_node: "sea-orm")
-    // There should still be only 1 edge
+    {
+        // Edge should be updated (from_node: "sea-query", to_node: "sea-orm")
+        assert!(
+            TestEdge::get_with_from_to(db, "depends", "sea-query", "sea-orm")
+                .await?
+                .is_some()
+        );
+        // There should still be only 1 edge
+        assert_eq!(TestEdge::get_all(db, "depends").await?.len(), 1);
+    }
 
     println!("# Node and edges updated successfully! #");
 
@@ -144,12 +196,15 @@ async fn main() -> Result<(), DbErr> {
         of: "depends".to_owned(),
         edge_content: MutateEdgeContentJson {
             from_node: Some("sea-query".to_owned()),
-            to_node: Some("sea-orm".to_owned()),
+            to_node: None,
         },
     }));
 
     Mutate::mutate(db, mutate_json, false).await?;
-    // Edge should be deleted (from_node: "sea-query", to_node: "sea-orm")
+    {
+        // Edge should be deleted (from_node: "sea-query", to_node: "sea-orm")
+        assert!(TestEdge::get_all(db, "depends").await?.is_empty());
+    }
 
     let mutate_json = MutateJson::delete(MutateDeleteJson::node(MutateNodeSelectorJson {
         of: "crate".to_owned(),
@@ -158,9 +213,79 @@ async fn main() -> Result<(), DbErr> {
     }));
 
     Mutate::mutate(db, mutate_json, false).await?;
-    // Node should be deleted (name: "sea-orm")
+    {
+        // Node should be deleted (name: "sea-orm")
+        assert!(
+            TestEdge::get_with_from_to(db, "depends", "sea-query", "sea-orm")
+                .await?
+                .is_none()
+        );
+    }
 
     println!("# Node and edges deleted successfully! #");
 
     Ok(())
+}
+
+#[derive(Debug, Clone, FromQueryResult)]
+struct TestNode {
+    name: String,
+    attr_version: String,
+}
+
+impl TestNode {
+    async fn get_with_name(db: &DbConn, of: &str, name: &str) -> Result<Option<Self>, DbErr> {
+        let stmt = Query::select()
+            .column(Alias::new("name"))
+            .column(Alias::new("attr_version"))
+            .from(Alias::new(&format_node_table_name(of)))
+            .and_where(Expr::col(Alias::new("name")).eq(name))
+            .to_owned();
+
+        let builder = db.get_database_backend();
+
+        Self::find_by_statement(builder.build(&stmt)).one(db).await
+    }
+}
+
+#[derive(Debug, Clone, FromQueryResult)]
+struct TestEdge {
+    from_node: String,
+    to_node: String,
+}
+
+impl TestEdge {
+    async fn get_with_from_to(
+        db: &DbConn,
+        of: &str,
+        from: &str,
+        to: &str,
+    ) -> Result<Option<Self>, DbErr> {
+        let stmt = Query::select()
+            .column(Alias::new("from_node"))
+            .column(Alias::new("to_node"))
+            .from(Alias::new(&format_edge_table_name(of)))
+            .cond_where(
+                Cond::all()
+                    .add(Expr::col(Alias::new("from_node")).eq(from))
+                    .add(Expr::col(Alias::new("to_node")).eq(to)),
+            )
+            .to_owned();
+
+        let builder = db.get_database_backend();
+
+        Self::find_by_statement(builder.build(&stmt)).one(db).await
+    }
+
+    async fn get_all(db: &DbConn, of: &str) -> Result<Vec<Self>, DbErr> {
+        let stmt = Query::select()
+            .column(Alias::new("from_node"))
+            .column(Alias::new("to_node"))
+            .from(Alias::new(&format_edge_table_name(of)))
+            .to_owned();
+
+        let builder = db.get_database_backend();
+
+        Self::find_by_statement(builder.build(&stmt)).all(db).await
+    }
 }
