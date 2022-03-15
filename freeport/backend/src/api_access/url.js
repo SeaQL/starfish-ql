44abdf2b0cd8a7195047b502c7d3a44b97561f4a
Billy Chan <ccw.billy.123@gmail.com>
2022-03-15 21:18:43 +0800

    Backend with `API_BASE_URL` & `API_AUTH_KEY` environment variable

--- freeport/backend/src/api_access/url.js
+++ freeport/backend/src/api_access/url.js
@@ -1,13 +1,17 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = process.env.API_BASE_URL ?? "http://127.0.0.1:8000";
 
 module.exports = {
     constructUrl: (endpoint, queryParams = {}) => {
         let url = BASE_URL + "/" + endpoint + "?";
 
+        let params = [];
         for (const [k, v] of Object.entries(queryParams)) {
-            url += `${k}=${v}`;
+            params.push(`${k}=${v}`);
+        }
+        if (process.env.API_AUTH_KEY) {
+            params.push(`auth=${process.env.API_AUTH_KEY}`);
         }
 
-        return url;
+        return url + params.join('&');
     }
 };
