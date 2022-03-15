7e8ce491022648915f9f3ea02315883092c59073
Billy Chan <ccw.billy.123@gmail.com>
2022-03-15 23:15:04 +0800

    Compatibility

--- freeport/backend/src/api_access/url.js
+++ freeport/backend/src/api_access/url.js
@@ -1,8 +1,14 @@
-const BASE_URL = process.env.API_BASE_URL ?? "http://127.0.0.1:8000";
+const BASE_URL = "http://127.0.0.1:8000";
 
 module.exports = {
     constructUrl: (endpoint, queryParams = {}) => {
-        let url = BASE_URL + "/" + endpoint + "?";
+        let url = '';
+        if (process.env.API_BASE_URL) {
+            url += process.env.API_BASE_URL;
+        } else {
+            url += BASE_URL;
+        }
+        url += "/" + endpoint + "?";
 
         let params = [];
         for (const [k, v] of Object.entries(queryParams)) {
