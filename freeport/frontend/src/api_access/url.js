cc570d186939f01f9823511d818d3cc2bb733870
Billy Chan <ccw.billy.123@gmail.com>
2022-03-16 00:22:37 +0800

    Testing

--- freeport/frontend/src/api_access/url.js
+++ freeport/frontend/src/api_access/url.js
@@ -1,7 +1,15 @@
 const BASE_URL = "http://localhost:8000";
 
 export const constructUrl = (endpoint) => {
-    let url = BASE_URL + "/" + endpoint;
+    let url = '';
+    if (process.env.API_BASE_URL) {
+        url += process.env.API_BASE_URL;
+    } else {
+        url += BASE_URL;
+    }
+    url += "/" + endpoint + "?";
+
+    console.log(process.env.API_BASE_URL);
 
     return url;
 };
