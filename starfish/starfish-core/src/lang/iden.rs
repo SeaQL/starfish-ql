a5eaadb43fcda250514dd367dffb510167b21bfe
Sanford Pun <punsanford@gmail.com>
2022-04-12 14:52:34 +0800

    Reusable iden in mutate/node.rs

--- starfish/starfish-core/src/lang/iden.rs
+++ starfish/starfish-core/src/lang/iden.rs
@@ -20,3 +20,47 @@ pub enum NodeIden {
     Name,
 }
 
+/// Reusable column identifiers for entities
+#[derive(Debug, Iden)]
+pub enum EntityIden {
+    /// The `entity` table
+    #[iden = "entity"]
+    Entity,
+    /// The `id` column of an entity
+    #[iden = "id"]
+    Id,
+    /// The `datatype` column of an entity
+    #[iden = "datatype"]
+    Datatype,
+    /// The `name` column of an entity
+    #[iden = "name"]
+    Name,
+}
+
+impl EntityIden {
+    /// Specify `self` as a column of the `entity` table
+    pub fn prefixed_with_table(self) -> (Self, Self) {
+        (Self::Entity, self)
+    }
+}
+
+/// Reusable column identifiers for entity attributes
+#[derive(Debug, Iden)]
+pub enum EntityAttrIden {
+    /// The `entity_attribute` table
+    #[iden = "entity_attribute"]
+    EntityAttribute,
+    /// The `name` column of an entity attribute
+    #[iden = "name"]
+    Name,
+    /// The `entity_id` column of an entity attribute
+    #[iden = "entity_id"]
+    EntityId,
+}
+
+impl EntityAttrIden {
+    /// Specify `self` as a column of the `entity_attribute` table
+    pub fn prefixed_with_table(self) -> (Self, Self) {
+        (Self::EntityAttribute, self)
+    }
+}
