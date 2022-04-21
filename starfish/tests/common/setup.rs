ea7356493b940da97f25c545a3e6d78e744435fa
Sanford Pun <punsanford@gmail.com>
2022-04-21 20:07:49 +0800

    Update tests/common/setup.rs to initialize testing db env for PostgreSQL & SQLite

--- starfish/tests/common/setup.rs
+++ starfish/tests/common/setup.rs
@@ -2,22 +2,47 @@ use sea_orm::{ConnectionTrait, Database, DatabaseConnection, DbBackend, Statemen
 use starfish_core::sea_orm;
 
 pub async fn setup(base_url: &str, db_name: &str) -> DatabaseConnection {
-    let url = format!("{}/mysql", base_url);
-    let db = Database::connect(&url).await.unwrap();
-    let _drop_db_result = db
-        .execute(Statement::from_string(
-            DbBackend::MySql,
-            format!("DROP DATABASE IF EXISTS `{}`;", db_name),
-        ))
-        .await;
+    let db = if cfg!(feature = "sqlx-mysql") {
+        let url = format!("{}/mysql", base_url);
+        let db = Database::connect(&url).await.unwrap();
+        let _drop_db_result = db
+            .execute(Statement::from_string(
+                DbBackend::MySql,
+                format!("DROP DATABASE IF EXISTS `{}`;", db_name),
+            ))
+            .await;
 
-    let _create_db_result = db
-        .execute(Statement::from_string(
-            DbBackend::MySql,
-            format!("CREATE DATABASE `{}`;", db_name),
-        ))
-        .await;
+        let _create_db_result = db
+            .execute(Statement::from_string(
+                DbBackend::MySql,
+                format!("CREATE DATABASE `{}`;", db_name),
+            ))
+            .await;
 
-    let url = format!("{}/{}", base_url, db_name);
-    Database::connect(&url).await.unwrap()
+        let url = format!("{}/{}", base_url, db_name);
+        Database::connect(&url).await.unwrap()
+    } else if cfg!(feature = "sqlx-postgres") {
+        let url = format!("{}/postgres", base_url);
+        let db = Database::connect(&url).await.unwrap();
+        let _drop_db_result = db
+            .execute(Statement::from_string(
+                DbBackend::Postgres,
+                format!("DROP DATABASE IF EXISTS \"{}\";", db_name),
+            ))
+            .await;
+
+        let _create_db_result = db
+            .execute(Statement::from_string(
+                DbBackend::Postgres,
+                format!("CREATE DATABASE \"{}\";", db_name),
+            ))
+            .await;
+
+        let url = format!("{}/{}", base_url, db_name);
+        Database::connect(&url).await.unwrap()
+    } else {
+        Database::connect(base_url).await.unwrap()
+    };
+
+    db
 }
